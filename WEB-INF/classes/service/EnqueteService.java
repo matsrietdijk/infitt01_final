69f8325369a242223445f8c10d1e7ba7591be605
matsrietdijk <matsrietdijk@gmail.com>
2013-02-05 20:13:40 +0100

    Get all enquetes joined by questions joined by choices added

--- WEB-INF/classes/service/EnqueteService.java
+++ WEB-INF/classes/service/EnqueteService.java
@@ -9,6 +9,7 @@ import javax.sql.*;
 
 import model.Enquete;
 import model.Question;
+import model.Choice;
 
 public class EnqueteService {
 	private List<Enquete> enquetes;
@@ -25,14 +26,33 @@ public class EnqueteService {
 				Context initContext = new InitialContext();
 				DataSource ds = (DataSource) initContext.lookup("java:/comp/env/jdbc/EnqueteDB");
 				Connection conn = ds.getConnection();
-				Statement stmt = conn.createStatement();
+				Statement estmt = conn.createStatement();
+				Statement qstmt = conn.createStatement();
+				Statement cstmt = conn.createStatement();
 
-				ResultSet rs = stmt.executeQuery("SELECT id, name FROM enquete");
-				while (rs.next()) {
-					this.enquetes.add(new Enquete(rs.getInt(1), rs.getString(2)));
+				ResultSet ers = estmt.executeQuery("SELECT id, name FROM enquete");
+				while (ers.next()) {
+					Enquete enquete = new Enquete(ers.getInt(1), ers.getString(2));
+					ResultSet qrs = qstmt.executeQuery("SELECT id, type, question FROM question WHERE enquete_id = " + ers.getInt(1));
+					while (qrs.next()) {
+						Question question = new Question(qrs.getInt(1), qrs.getInt(2), enquete, qrs.getString(3));
+						if (qrs.getInt(2) == 0) {
+							ResultSet crs = cstmt.executeQuery("SELECT id, choice FROM choice WHERE question_id = " + qrs.getInt(1));
+							while (crs.next()) {
+								Choice choice = new Choice(crs.getInt(1), question, crs.getString(2));
+								question.addChoice(choice);
+							}
+							crs.close();
+							cstmt.close();
+						}
+						enquete.addQuestion(question);
+					}
+					this.enquetes.add(enquete);
+					qrs.close();
+					qstmt.close();
 				}
-				rs.close();
-				stmt.close();
+				ers.close();
+				estmt.close();
 				conn.close();
 			} catch (NamingException ne) {
 				System.out.println("Datasource niet gevonden! "+ne);
